d2ebc9d50c7490f07240d0b54e664807eb59e034
Tojaman <zzoon5020@naver.com>
2024-11-01 19:01:55 +0900

    feat: Logout 기능 구현
    로그아웃 요청 시 Redis에 있는 RefreshToken 제거하고 RefreshToken에 null값 넣고 쿠키로 만들어서 응답

---
+++ backend/src/main/java/com/example/CustomLecture/jwt/CustomLogoutFilter.java
@@ -0,0 +1,102 @@
+package com.example.CustomLecture.jwt;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.ExpiredJwtException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CustomLogoutFilter extends GenericFilterBean {
+    private final JWTUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public CustomLogoutFilter(JWTUtil jwtUtil, RedisTemplate<String, String> redisTemplate) {
+        this.jwtUtil = jwtUtil;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
+    }
+
+    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        //path and method verify
+        String requestUri = request.getRequestURI();
+        if (!requestUri.matches("^\\/logout$")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String requestMethod = request.getMethod();
+        if (!requestMethod.equals("POST")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //get refresh token
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh"))
+                refresh = cookie.getValue();
+        }
+
+        if (refresh == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        //expired check
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+            //response status code
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
+        String type = jwtUtil.getType(refresh);
+
+        if (!type.equals("refresh")) {
+            //response status code
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // Redis에 RefreshToken이 저장되어 있는지 확인
+        String username = jwtUtil.getUsername(refresh);
+        String refreshToken = redisTemplate.opsForValue().get(username);
+        if (refreshToken == null || !refreshToken.equals(refresh)) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // Redis에서 RefreshToken 제거
+        redisTemplate.delete(username);
+
+        // 추후 AccessToken 블랙리스트로 추가하여 만료 시간이 남은 AccessToken으로 요청이 오는 보안 문제 해결 예정
+
+        // RefreshToken에 null 값을 만들고 Cookie로 만들어 응답
+        Cookie cookie = new Cookie("refresh", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
